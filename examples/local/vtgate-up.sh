f2dba79f0bd690e1753569e1a8fa137f357a6db0
Jun Sumida <jun.sumida@mixi.co.jp>
2015-12-01 18:24:37 +0900

    up vtgate example with grpc

--- examples/local/vtgate-up.sh
+++ examples/local/vtgate-up.sh
@@ -6,17 +6,21 @@ set -e
 
 cell='test'
 port=15001
+grpcport=15002
 
 script_root=`dirname "${BASH_SOURCE}"`
 source $script_root/env.sh
+
+# -service_map 'bsonrpc-vt-vtgateservice' \
 
 # Start vtgate.
 $VTROOT/bin/vtgate \
   -log_dir $VTDATAROOT/tmp \
   -port $port \
+  -grpc_port $grpcport=15002 \
   -cell $cell \
   -tablet_protocol grpc \
-  -service_map 'bsonrpc-vt-vtgateservice' \
+  -service_map 'grpc-vtgateservice' \
   -pid_file $VTDATAROOT/tmp/vtgate.pid \
   > $VTDATAROOT/tmp/vtgate.out 2>&1 &
 
